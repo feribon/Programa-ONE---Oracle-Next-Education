919dce6bfb4cb08ff54b3b233a80ea03eb0fa75a
Felipe Grassi Ribon <felipe.gribon@hotmail.com>
2024-01-29 23:19:58 -0300

    desafio 08

---
+++ desafio08/app.js
@@ -0,0 +1,14 @@
+let listaGenerica =[]
+
+let linguagensDeProgramacao = ['Javascript','C','C++','Kotlin','Pytohn']
+
+linguagensDeProgramacao.push('Java')
+linguagensDeProgramacao.push('Ruby')
+linguagensDeProgramacao.push('GoLang')
+
+let nomes =['Felipe','Grassi','Ribon']
+console.log(nomes[0])
+
+console.log(nomes[1])
+
+console.log(nomes[2])
